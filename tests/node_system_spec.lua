374729429d1ae83cf508f984adb5bfd90ee0e347
Nico <cubus.ger@googlemail.com>
2025-07-24 19:44:45 +0200

    Integrate resource production across systems

--- tests/node_system_spec.lua
+++ tests/node_system_spec.lua
@@ -1,11 +1,14 @@
-_G.printf = function() end
-local node = dofile('gamma_walo/gamedata/scripts/node_system.script')
-local pool = dofile('gamma_walo/gamedata/scripts/resource_pool.script')
+require('tests.spec_helper')
+local node = require('node_system')
+local pool = require('resource_pool')
+local res  = require('resource_system')
 
 describe('node_system', function()
     before_each(function()
         node.nodes = {}
         pool.levels = {}
+        res.nodes = {}
+        res.factions = {}
     end)
 
     it('captures and specializes nodes', function()
@@ -13,7 +16,8 @@ describe('node_system', function()
         node.capture_node('n1', 'duty')
         node.establish_node('n1', node.STATE.resource)
         node.specialize_node('n1', 'scrap')
-        node.tick_production(pool)
+        node.tick_production(pool, res)
         assert.equals(1, pool.get_resource('duty', 'scrap'))
+        assert.equals(1, res.nodes['n1'].stock)
     end)
 end)
